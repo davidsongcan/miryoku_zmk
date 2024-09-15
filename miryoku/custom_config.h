a3d274883051acec201770049485f2dc0fa6ea9c
David Song <125702430+davidsongcan@users.noreply.github.com>
2024-09-15 00:49:37 -0500

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,9 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
-
+#define MIRYOKU_ALPHAS_default
+#define MIRYOKU_TAP_default
+#define MIRYOKU_EXTRA_QWERTY
+#define MIRYOKU_NAV_INVERTEDT
+#define MIRYOKU_CLIPBOARD_WIN
+#define MIRYOKU_LAYERS_FLIP
+#define MIRYOKU_MAPPING_defult
